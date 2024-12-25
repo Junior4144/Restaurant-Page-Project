208e6c0da798e7e2fda955d83328d3498e62ff2d
Junior4144 <94208651+Junior4144@users.noreply.github.com>
2024-12-24 23:51:11 -0600

    Feat: menu section basic layout implemented

--- src/page-load.js
+++ src/page-load.js
@@ -26,31 +26,63 @@ export function home(){
 export function menu(){
     const content = document.getElementById('content');
 
-    const menu = document.createElement('div');
-    menu.classList.add('menu')
+    const menu_container = document.createElement('div');
+    menu_container.classList.add('menu-container')
 
     const headLine = document.createElement('div');
     headLine.classList.add("headLine");
     headLine.textContent = "Menu";
 
-    const drinksMenu = document.createElement('div');
-    drinksMenu.classList.add("drinkMenu");
+    //drinks title
+    const drink_title = document.createElement('div');
+    drink_title.classList.add("drink-title");
+    drink_title.textContent = "Drinks"
+    //drinks menu
 
 
-    
-    const drink1 = document.createElement('div');
-    drink1.textContent = "Sweet Tea";
+    const drinks_container = document.createElement('div');
+    drinks_container.classList.add("drinks-container");
 
-    const drink2 = document.createElement('div');
-    drink2.textContent = "Coca-Cola";
+    const drink_card_1 = document.createElement('div');
+    drink_card_1.classList.add('card')
+    drink_card_1.textContent = "Sweet Tea";
 
-    drinksMenu.appendChild(drink1);
-    drinksMenu.appendChild(drink2);
+    const drink_card_2 = document.createElement('div');
+    drink_card_2.classList.add('card')
+    drink_card_2.textContent = "Coca-Cola";
 
-    menu.appendChild(headLine);
-    menu.appendChild(drinksMenu);
+    drinks_container.appendChild(drink_card_1);
+    drinks_container.appendChild(drink_card_2);
 
-    content.appendChild(menu)
+    // food title
+    const food_title = document.createElement('div');
+    food_title.classList.add("food-title");
+    food_title.textContent = "Food";
+
+    // food menu
+    const food_container = document.createElement('div');
+    food_container.classList.add("food-container");
+
+    const food_card_1 = document.createElement('div');
+    food_card_1.textContent = "Tacos";
+    food_card_1.classList.add('card');
+
+    const food_card_2 = document.createElement('div');
+    food_card_2.textContent = "Steak";
+    food_card_2.classList.add('card')
+
+    food_container.appendChild(food_card_1);
+    food_container.appendChild(food_card_2);
+
+    //adding to menu
+
+    menu_container.appendChild(headLine);
+    menu_container.appendChild(drink_title);
+    menu_container.appendChild(drinks_container);
+    menu_container.appendChild(food_title);
+    menu_container.appendChild(food_container);
+
+    content.appendChild(menu_container);
     
 
 
